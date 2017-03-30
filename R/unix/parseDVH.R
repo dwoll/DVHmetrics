dd72580ab9005b52c2f43194b0f68818887e6274
Daniel Wollschlaeger <dwollschlaeger@gmail.com>
2017-03-30 14:35:02 +0200

    change ProSoma patID

--- R/unix/parseDVH.R
+++ R/unix/parseDVH.R
@@ -18,9 +18,9 @@ parseDVH <- function(x, type=c("Eclipse", "Cadplan", "Masterplan",
         } else if(type == "HiArt") {
             gsub("[^a-z0-9]", "\\1", tempfile(pattern="", tmpdir=""))
         } else if(type == "ProSoma") {
-            patName <- paste0(substr(trimWS(rev(strsplit(txt[1], ",")[[1]])), 1, 1))
+            patName <- paste(substr(trimWS(rev(strsplit(txt[1], ",")[[1]])), 1, 1), collapse="")
             randStr <- gsub("[^a-z0-9]", "\\1", tempfile(pattern="", tmpdir=""))
-            paste(randStr, patName, collapse="_")
+            paste(randStr, patName, sep="_")
         } else if(type != "Pinnacle") {
             IDline <- txt[grep("^(Patient ID|Case|# Project)[[:blank:]]*:", txt)]
             IDres  <- sub("^.+?:[[:blank:]]+([[:alnum:][:punct:][:blank:]]+$)", "\\1", IDline, perl=TRUE)

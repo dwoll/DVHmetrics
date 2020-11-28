09e6556246a61ac32713ef68b93aa050f78cd669
Daniel Wollschlaeger <dwoll@kuci.org>
2020-11-28 13:08:16 +0100

    bs4Dash shiny app

---
+++ inst/DVHshiny_bs4Dash/app_ui_tab_data.R
@@ -0,0 +1,35 @@
+fluidPage(
+    fluidRow(
+        bs4Box(
+            title="Enter data",
+            width=4,
+            radioButtons("DVHin", label=h4("Enter data"),
+                         list("Use built-in data"=1,
+                              "Upload DVH file"=2)),
+            conditionalPanel(condition="input.DVHin == '2'",
+                             h5("Upload DVH file: "),
+                             radioButtons("DVHtype", "DVH file format:",
+                                          list("Eclipse"=1, "CadPlan"=2, "MasterPlan"=3,
+                                               "Pinnacle3"=4, "Monaco"=5, "HiArt"=6,
+                                               "RayStation"=7, "ProSoma"=8, "PRIMO"=9)),
+                             fileInput("DVHupload", "Select DVH file:", multiple=TRUE),
+                             # radioButtons("fileenc", "File encoding:",
+                             #              list("Default"=1, "UTF-8"=2, "UTF-8-BOM"=3)),
+                             radioButtons("DVHplanInfo", "Information encoded in plan:",
+                                          list("None"=1, "Prescribed dose"=2)),
+                             checkboxGroupInput("DVHreadOpts", label=NULL,
+                                                choices=c("Add to existing data"="DVHadd",
+                                                          "Use Course for ID"="DVHcourse",
+                                                          "Struct volume from DVH"="volume_from_dvh"))),
+            actionButton("applyData", "Apply"),
+            radioButtons("DVHverbose", "",
+                         list("Short info on DVHs"=1,
+                              "Detailed info on DVHs"=2))
+        ),
+        bs4Box(
+            title="Information from imported DVH file(s)",
+            width=8,
+            verbatimTextOutput("DVHinfo")
+        )
+    )
+)
---
+++ inst/DVHshiny_bs4Dash/app_ui_tab_show_dvh.R
@@ -0,0 +1,38 @@
+fluidPage(
+    fluidRow(
+        bs4Box(
+            title="Plot options",
+            width=4,
+            radioButtons("plotByPat", label=h5("Plot by patient or by structure"),
+                         list("By patient"=1,
+                              "By structure"=2)),
+            uiOutput("plotSelPat"),
+            actionButton("plotSelPatAll", label="(De)Select All"),
+            uiOutput("plotSelStruct"),
+            actionButton("plotSelStructAll", label="(De)Select All"),
+            radioButtons("plotPlotVol", label=h5("Plot relative/absolute volume"),
+                         list("Relative volume"=1,
+                              "Absolute volume"=2)),
+            radioButtons("plotType", label=h5("DVH type"),
+                         list("Cumulative"=1,
+                              "Differential"=2)),
+            checkboxInput("plotMSD", "Show M + SD areas", FALSE),
+            sliderInput("plotThreshVol", label=h5("Threshold volume"),
+                        min=0, max=100, value=1)
+        ),
+        bs4Box(
+            title="Show DVH diagrams",
+            width=8,
+            downloadButton("saveDVHPDF", "Save as PDF"),
+            downloadButton("saveDVHJPG", "Save as JPEGs (zipped to one file)"),
+            #plotOutput("DVHplotOrg"),
+            uiOutput("DVHplot"),
+            downloadButton("saveDVHMSD", "Save DVH Mean + SD as text file"),
+            inputPanel(
+                radioButtons("saveDVHDec", "Decimal separator:",
+                             list("."=1, ","=2)),
+                radioButtons("saveDVHSep", "Column separator:",
+                             list("\\t (tab)"=1, "' ' (space)"=2, ", (comma)"=3, "; (semicolon)"=4)))
+        )
+    )
+)

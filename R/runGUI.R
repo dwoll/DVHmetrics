c696dcf449de587717026b6e07415d4dd02e7824
Daniel Wollschlaeger <dwoll@kuci.org>
2021-04-09 12:26:16 +0200

    fix

--- R/runGUI.R
+++ R/runGUI.R
@@ -15,7 +15,7 @@ function(...) {
                 shiny::runApp(appDir, ...)
             } else {
                 appDir_bs4Dash_old <- paste0(appDir, "_bs4Dash_05")
-                shiny::runApp(appDir, ...)
+                shiny::runApp(appDir_bs4Dash_old, ...)
             }
         } else {
             warning("Package 'bs4Dash' not found - running legacy version")

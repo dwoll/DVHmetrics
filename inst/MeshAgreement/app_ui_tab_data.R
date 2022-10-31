ceb70c294f97a5cdc2400bace3e660d5c22dac81
Wollschlaeger <dwollsch@Uni-Mainz.DE>
2022-10-31 11:30:12 +0100

    cosmetic app changes

--- inst/MeshAgreement/app_ui_tab_data.R
+++ inst/MeshAgreement/app_ui_tab_data.R
@@ -2,7 +2,7 @@ fluidPage(
     fluidRow(
         column(
             width=12,
-            box(title="Load mesh data",
+            box(title="Load 3D mesh data",
                 width=12,
                 # height="650px",
                 status=NULL,
@@ -12,7 +12,7 @@ fluidPage(
                 radioButtons("meshes_in",
                              label=NULL,
                              list("Use built-in data"=1,
-                                  "Upload mesh files"=2)),
+                                  "Upload 3D mesh files"=2)),
                 uiOutput("ui_select_files"),
                 actionButton("apply_file_sel", "Apply")
             )
@@ -29,7 +29,8 @@ fluidPage(
                 maximizable=FALSE,
                 collapsible=FALSE,
                 p("Note: If information on mesh volume and mesh centroid are missing ('NA'), the mesh is not proper.",
-                  "Likely, the mesh does not bound a volume. Some agreement measures will be unavailable."),
+                  "Likely, the mesh does not bound a volume, or has self-intersections. Some agreement measures will be unavailable.",
+                  "Maybe, this can be fixed using surface reconstruction on import."),
                 htmlOutput("print_mesh_info")
             )
         )

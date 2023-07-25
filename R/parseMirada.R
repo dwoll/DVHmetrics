32abb9a171ac9897b683440fc5df53f9081c85c9
Wollschlaeger <dwollsch@Uni-Mainz.DE>
2023-07-25 12:27:54 +0200

    add Mirada support

--- R/parseMirada.R
+++ R/parseMirada.R
@@ -4,24 +4,47 @@
 parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
     planInfo <- as.character(planInfo)
     dots     <- list(...)
-    
+
     ## extract file header and header info
     ## Mirada file does not have a PatID
     header1  <- strsplit(x[1], "\t")[[1]]
     patName  <- header1[2]
     DVHdate  <- tryCatch(as.Date(strptime(header1[3], "%m/%d/%Y")),
                          error=function(e) { NA_character_ })
-    
+
     patID      <- gsub("[^a-z0-9]", "\\1", tempfile(pattern="", tmpdir="")) # as.character(trunc(runif(1, 0, 10000001)))
     doseUnit   <- NA_character_
     volumeUnit <- NA_character_
     doseRx     <- NA_real_
 
+    ## Mirada provides no information on prescribed dose
+    ## check if additional information is given via option mirada
+    dots <- list(...)
+    if(hasName(dots, "mirada")) {
+        info <- dots[["mirada"]]
+
+        if(hasName(info, "doseRx")) {
+            drxu <- info[["doseRx"]]
+            doseRxUnit <- toupper(sub("^[.[:digit:][:blank:]]+(c?Gy).*$", "\\1",
+                                      drxu, perl=TRUE, ignore.case=TRUE))
+
+            if(!grepl("^(GY|CGY)$", doseRxUnit)) {
+                warning("Could not determine dose Rx unit")
+                doseRxUnit <- NA_character_
+            }
+
+            drx <- sub("^([.[:digit:]]+)[[:blank:]]*c?Gy.*$", "\\1",
+                       drxu, perl=TRUE, ignore.case=TRUE)
+            doseRx <- as.numeric(drx)
+        }
+    }
+
     ## extract actual DVH columns
-    idx_sep   <- which(!nzchar(trimws(x)))[1]
-    DVHspan   <- x[(idx_sep+1):(length(x)-1)]
+    idx_sep   <- which(!nzchar(trimws(x)))[1] # blank line between header and dvh
+    DVHspan   <- x[(idx_sep+1):length(x)]
     ## construct proper DVH names
     DVHnames0 <- strsplit(DVHspan[1], "\t")[[1]]
+    ## columns for dose and volume
     dose_cols <- seq(1L, length(DVHnames0)-1L, by=2L)
     vol_cols  <- dose_cols+1L
     isVolRel  <- all(grepl("^% of volume$", DVHnames0[vol_cols]))
@@ -30,7 +53,7 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
     struct_vols   <- as.numeric(gsub("^[[:alnum:]_-]+[[:blank:]]+\\(([[:digit:].]+).+\\)$", "\\1", struct_names0))
     volumeUnit0   <- gsub("^[[:alnum:]_-]+[[:blank:]]+\\([[:digit:].]+(.+)\\)$", "\\1", struct_names0)
     volumeUnit1   <- unique(substr(volumeUnit0, 1L, 2L))
-    
+
     stopifnot(length(volumeUnit1) == 1L)
     volumeUnit <- if(volumeUnit1 == "cm") {
         "CC"
@@ -40,7 +63,7 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
 
     DVHall <- read.table(text=DVHspan[-1], header=FALSE, sep="\t",
                          stringsAsFactors=FALSE, comment.char="")
-    
+
     ## DVH structures are given in wide format -> split
     getDVH <- function(idx_dose, idx_vol, structName, structVol, info) {
         dvh <- data.matrix(DVHall[ , idx_dose:idx_vol])
@@ -50,10 +73,10 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
         } else {
             c("dose", "volume")
         }
-        
+
         ## check if dose is increasing
         stopifnot(isIncreasing(dvh))
-        
+
         ## add absolute/relative volume
         dvh <- if(info$isVolRel) {
             cbind(dvh,
@@ -69,15 +92,15 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
             } else {
                 NA_real_
             }
-            
+
             cbind(dvh,
                   volumeRel=100*(dvh[ , "volume"] / structVol),
                   doseRel=NA_real_)
         }
-        
+
         ## differential or cumulative DVH
         DVHtype <- dvhType(dvh)
-        
+
         DVH <- list(dvh=dvh,
                     patName=info$patName,
                     patID=info$patID,
@@ -94,7 +117,7 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
                     doseMed=NA_real_,
                     doseMode=NA_real_,
                     doseSD=NA_real_)
-        
+
         ## convert differential DVH (not per unit dose!) to cumulative
         ## and add differential DVH separately
         if(DVHtype == "differential") {
@@ -102,7 +125,7 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
                                   toDoseUnit="asis", perDose=FALSE)
             DVH$dvhDiff <- dvh
         }
-        
+
         ## set class
         class(DVH) <- "DVHs"
         return(DVH)
@@ -113,10 +136,10 @@ parseMirada <- function(x, planInfo=FALSE, courseAsID=FALSE, ...) {
                  patName=patName,
                  date=DVHdate,
                  doseRx=doseRx,
-                 doseUnit=doseUnit,
+                 doseUnit=doseRxUnit,
                  volumeUnit=volumeUnit,
                  isVolRel=isVolRel)
-    
+
     dvhL <- Map(getDVH,
                 dose_cols, vol_cols,
                 struct_names,
